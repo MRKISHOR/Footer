840ae81dcb3d1a7e0f2ed84ddd2068763d04f712
mrkishor <mrkishor4@gmail.com>
2023-05-01 19:06:59 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,248 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <footer>
+
+        <section class="globalsite cmp-globalsite-globalcontextualfooter">
+
+          <div class="contextual-footer smoky-background-color">
+            <div class="top-footer-nav grid-norm">
+              <div class="row">
+                <div class="col-md-2 col-sm-12">
+                  <a href="/en">
+                    <img alt="Sheer" src="https://www.sheeranalyticsandinsights.com/static/sai_logo-8bdcb86c43808dfb831384efccd61b52.png" width="185" height="45" class="img-responsive lazyload" />
+                  </a>
+                </div>
+                {/* <!-- desktop top links--> */}
+
+                <div class="col-md-3 social-share hide-tablet hide-mobile hide-mobile-sm">
+                  <a href="https://www.youtube.com" class="social-link">
+                    <span class="gcom-icon-social-youtube"></span>
+                  </a>
+
+                  <a href="https://twitter.com" class="social-link">
+                    <span class="gcom-icon-social-twitter"></span>
+                  </a>
+
+                  <a href="https://www.linkedin.com" class="social-link">
+                    <span class="gcom-icon-social-linkedin"></span>
+                  </a>
+
+                  <a href="https://www.facebook.com" class="social-link">
+                    <span class="gcom-icon-social-facebook"></span>
+                  </a>
+
+                  <a href="https://www.instagram.com" class="social-link">
+                    <span class="gcom-icon-social-instagram"></span>
+                  </a>
+                </div>
+              </div>
+              {/* <!-- mobile top links --> */}
+
+            </div>
+            <div class="grid-full no-top-bottom">
+              <div class="grid-norm no-top-bottom">
+                <div class="row">
+                  <div class="col-md-12 xml-container">
+                    <ul class="nav navbar-nav nav-left hide-mobile-sm hide-tablet hide-mobile">
+                      <li class="mg-r30 li-style">
+                        <span href="#" class="nav-link primary-link">About SAI </span>
+                        <div class="row">
+                          <div class="sub-nav-col col-sm-6">
+                            <span class="mg-b15">Who We Are</span>
+                            <ul class="nav">
+                              <li>
+                                <a href="https://www.sheeranalyticsandinsights.com/about-us" class="nav-link" data-sublink="About Us" data-nav-category="About Gartner" data-nav-sub-category="Who We Are">
+                                  About Us
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Corporate Responsibility" data-nav-category="About Gartner" data-nav-sub-category="Who We Are">
+                                  Corporate Responsibility
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Investor Relations" data-nav-category="About Gartner" data-nav-sub-category="Who We Are">
+                                  Investor Relations
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Newsroom" data-nav-category="About Gartner" data-nav-sub-category="Who We Are">
+                                  Newsroom
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+
+                          <div class="sub-nav-col col-sm-6">
+                            <span class="mg-b15">What We Do</span>
+                            <ul class="nav">
+                              <li>
+                                <a href="/en/products" class="nav-link" data-sublink="Research &amp; Advisory" data-nav-category="About Gartner" data-nav-sub-category="What We Do">
+                                  Research &amp; Advisory
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="" class="nav-link" data-sublink="Conferences" data-nav-category="About Gartner" data-nav-sub-category="What We Do">
+                                  Conferences
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Consulting" data-nav-category="About Gartner" data-nav-sub-category="What We Do">
+                                  Consulting
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Digital Markets" data-nav-category="About Gartner" data-nav-sub-category="What We Do">
+                                  Digital Markets
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="mg-r30 li-style">
+                        <span href="#" class="nav-link primary-link">Get in Touch</span>
+                        <div class="row">
+                          <div class="sub-nav-col col-sm-6">
+                            <span class="mg-b15">Contact</span>
+                            <ul class="nav">
+                              <li>
+                                <a href="https://www.sheeranalyticsandinsights.com/contact-us" class="nav-link" data-sublink="Contact Us" data-nav-category="Get in Touch" data-nav-sub-category="Contact">
+                                  Contact Us
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="/en/become-a-client?bacCtaId=bacFooterCtaBecomeAClient" class="nav-link" data-sublink="Become a Client" data-nav-category="Get in Touch" data-nav-sub-category="Contact">
+                                  Become a Client
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Office Locations" data-nav-category="Get in Touch" data-nav-sub-category="Contact">
+                                  Office Locations
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href=" " class="nav-link" data-sublink="Technical Support" data-nav-category="Get in Touch" data-nav-sub-category="Contact">
+                                  Technical Support
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+
+                          <div class="sub-nav-col col-sm-6">
+                            <span class="mg-b15">Careers</span>
+                            <ul class="nav">
+                              <li>
+                                <a href="https://www.sheeranalyticsandinsights.com/" class="nav-link" data-sublink="Why Gartner" data-nav-category="Get in Touch" data-nav-sub-category="Careers">
+                                  Why Sheer
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="https://www.sheeranalyticsandinsights.com/career" class="nav-link" data-sublink="Search Careers" data-nav-category="Get in Touch" data-nav-sub-category="Careers">
+                                  Search Careers
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="" class="nav-link" data-sublink="Our Culture" data-nav-category="Get in Touch" data-nav-sub-category="Careers">
+                                  Our Culture
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="" class="nav-link" data-sublink="Careers Blog" data-nav-category="Get in Touch" data-nav-sub-category="Careers">
+                                  Careers Blog
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="mg-r30 li-style">
+                        <span href="#" class="nav-link primary-link">Latest Insights</span>
+                        <div class="row">
+                          <div class="sub-nav-col col-sm-12">
+                            <span class="mg-b15">Resources</span>
+                            <ul class="nav">
+                              <li>
+                                <a href="/en/insights" class="nav-link" data-sublink="Select Insights" data-nav-category="Latest Insights" data-nav-sub-category="Resources">
+                                  Select Insights
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="/en/webinars" class="nav-link" data-sublink="Webinars" data-nav-category="Latest Insights" data-nav-sub-category="Resources">
+                                  Webinars
+                                </a>
+                              </li>
+                              <li>
+                                <a href="/en/glossary" class="nav-link" data-sublink="Glossary" data-nav-category="Latest Insights" data-nav-sub-category="Resources">
+                                  Glossary
+                                </a>
+                              </li>
+
+                              <li>
+                                <a href="/en/customer-success-stories" class="nav-link" data-sublink="Client Stories" data-nav-category="Latest Insights" data-nav-sub-category="Resources">
+                                  Client Stories
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="footer-bottom-nav grid-norm">
+              <div class="row">
+                <div class="col-md-6 col-sm-12 col-xs-12 mg-tab">
+                  <span>
+                    <a class="footer-bottom-link" data-en-title="Policies" href="/en/about/policies/overview" title="Policies">Policies</a>
+                  </span>
+
+                  <span>
+                    <a class="footer-bottom-link" data-en-title="Privacy Policy" href="https://www.sheeranalyticsandinsights.com/privacy-policy" title="Privacy Policy">Privacy Policy</a>
+                  </span>
+
+                  <span>
+                    <a class="footer-bottom-link" data-en-title="Terms of Use" href="https://www.sheeranalyticsandinsights.com/terms-of-use" title="Terms of Use">Terms of Use</a>
+                  </span>
+
+                  <span>
+                    <a class="footer-bottom-link" data-en-title="Ombudsman" href="/en/about/ombuds" title="Ombuds">Ombuds</a>
+                  </span>
+                </div>
+                <div class="col-md-2 hide-mobile hide-mobile-sm">
+                </div>
+                <div class="col-md-4 hide-mobile hide-mobile-sm copyright hide-tablet">
+                  <p class="copyright-text flt-right">
+                    &copy;2023&nbsp;Sheer, Inc. and/or its affiliates. All rights reserved.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </footer>
+    </>
   );
 }
 
